7f5ebe0975cfdbb7fe3adbc61846e71dec0a1e9b
emilyqin05 <123emilyqin@gmail.com>
2024-09-02 14:28:08 -0700

    handling command line arguments

--- yolo_opencv.py
+++ yolo_opencv.py
@@ -1,3 +1,11 @@
 import cv2
 import argparse
 import numpy as np
+
+# command line args
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--image', required = True, help = 'path to input image') #add_argument( <command-line flags>, <requirement to run>, description of argument>)
+ap.add_argument('-c', '--config', required = True, help = 'path to yolo config file')
+ap.add_argument('-w', '--weights', required = True, help = 'path to yolo pre-trained weights')
+ap.add_argument('cl', '--classes', required = True, help = 'path to text file containing class names')
+args = ap.parse_args()
