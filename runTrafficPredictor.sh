56463dfa3dd83f2a24c08bfb54360647ff1af5a0
emilyqin05 <123emilyqin@gmail.com>
2024-09-29 23:39:30 -0700

    Delete runTrafficPredictor.sh

--- runTrafficPredictor.sh
+++
@@ -1,6 +0,0 @@
-
-#!/bin/zsh
-cd /Users/emilyqin/Desktop/traffic-predictor
-source /Users/emilyqin/.virtualenvs/cv/bin/activate
-env > /Users/emilyqin/Desktop/traffic-predictor/env_variables.log
-/Users/emilyqin/.virtualenvs/cv/bin/python /Users/emilyqin/Desktop/traffic-predictor/main.py >> /Users/emilyqin/Desktop/traffic-predictor/logfile.log 2>> /Users/emilyqin/Desktop/traffic-predictor/error.log
